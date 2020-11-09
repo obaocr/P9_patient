f428f900863a7fc14aaff37325d0c2c29d869b28
BARBERIS Olivier <olivier.barberis@axa.fr>
2020-11-09 17:16:14 +0100

    init

---
+++ src/main/java/com/ocr/p9_patient/P9PatientApplication.java
@@ -0,0 +1,13 @@
+package com.ocr.p9_patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P9PatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P9PatientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ocr/p9_patient/P9PatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocr.p9_patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P9PatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
